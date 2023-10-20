03a1d89cec69aaea19f96489da4b1b0401bd57c7
Funny <dquesadaa2004@gmail.com>
2023-10-20 09:54:57 -0600

    .

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Barberos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto;
+
+/**
+ *
+ * @author dques
+ */
+public class Barberos {
+    public String nombreBarbero="";
+    public int almuerzoBarbero=0;
+    
+    public String getNombreBarbero(){
+        return nombreBarbero;
+    }
+    
+    public void setBarbero(String nombre, int almuerzo){
+        this.nombreBarbero=nombre;
+        this.almuerzoBarbero=almuerzo;
+        
+}
+    
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Grupo #4 Conformado por:
+Carlos Pereira Cerdas
+Daniel Quesada Aguilar
+Keylin Sibaja Quiros
+Jose Pablo Vega Valverde
